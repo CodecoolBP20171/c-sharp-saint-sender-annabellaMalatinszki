b1cceeaede22b67ab51501ae16b8daeb3d92abdc
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-28 19:37:31 +0100

    Implement the MessageHandler class storing the list of emails

---
+++ SaintSender/SaintSender/MessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender
+{
+    class MessageHandler
+    {
+        Email[] messages;
+
+        public Email[] GetMessages()
+        {
+            return messages;
+        }
+    }
+}
